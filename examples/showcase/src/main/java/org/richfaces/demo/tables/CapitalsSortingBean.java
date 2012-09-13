948eb98689f0a47b9be9803b76f86620e522e815
Lukas Fryc <lfryc@redhat.com>
2012-09-13 10:38:06 +0200

    RF-12466: aligned with API changes

--- examples/showcase/src/main/java/org/richfaces/demo/tables/CapitalsSortingBean.java
+++ examples/showcase/src/main/java/org/richfaces/demo/tables/CapitalsSortingBean.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-import org.richfaces.component.SortOrder;
 import org.richfaces.demo.tables.model.capitals.Capital;
+import org.richfaces.ui.iteration.SortOrder;
 
 @ManagedBean
 @ViewScoped
